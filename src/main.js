b1d08a6d3fb909df018c75985331fa8fd53c1be0
reykhambhaita <rey.khambhaita819333@gmail.com>
2024-12-23 11:46:37 +0530

    feat: add export and preview buttons for tabs

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+//signature preview
 const textField = document.getElementById('textField');
 const signaturePrev = document.querySelector('.signature-prev');
 
@@ -13,3 +14,21 @@ textField.addEventListener('input', (event) => {
     signaturePrev.textContent = '';
   }
 })
+
+//export and preview tabs
+
+const tabButtons = document.querySelectorAll('.tab-button');
+const tabContents = document.querySelectorAll('.tab-content');
+
+tabButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const tabName = button.getAttribute('data-tab');
+
+
+    tabButtons.forEach(btn => btn.classList.remove('active'));
+    tabContents.forEach(content => content.classList.remove('active'));
+
+    button.classList.add('active');
+    document.querySelector(`.tab-content[data-tab="${tabName}"]`).classList.add('active');
+  })
+});
